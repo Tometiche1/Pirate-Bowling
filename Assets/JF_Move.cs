4f7288174d7c8f914e55031c55c9e02525d8b948
Nicholas Zeman <nbzeman@gmail.com>
2014-10-22 14:32:20 -0400

    Getting Environment ready

---
+++ Assets/JF_Move.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class JF_Move : MonoBehaviour {
+
+	private float v;
+	private float h;
+	public float rotMult = 1;
+	public float transMult = 1;
+	public float rollMult;
+	public Transform roller;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		v = Input.GetAxis ("Vertical");
+		h = Input.GetAxis ("Horizontal");
+
+		transform.Translate (0, 0, v * transMult);
+		transform.Rotate (0, h * rotMult, 0);
+		roller.Rotate (v * rollMult, 0, 0); 
+	
+	}
+}
