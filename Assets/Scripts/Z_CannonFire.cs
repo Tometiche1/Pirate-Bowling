ef60ac83ffd951d04fc607452c9b1254849fc3b9
Nicholas Zeman <nbzeman@gmail.com>
2014-10-22 13:20:39 -0400

    Initializing Repo

---
+++ Assets/Scripts/Z_CannonFire.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Z_CannonFire : MonoBehaviour {
+
+	//global variables
+
+	public GameObject cannonBall;
+	public float zforce;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void OnTriggerEnter(Collider thing){
+
+		print (thing.gameObject.name + " hit this trigger");
+		if (thing.gameObject.tag == "Player"){
+
+			//cannonball fires at player
+			cannonBall.rigidbody.constraints = RigidbodyConstraints.None;
+			cannonBall.rigidbody.AddRelativeForce(0,0,zforce*1000);
+			audio.Play();
+
+		}
+						;
+
+	}
+}
